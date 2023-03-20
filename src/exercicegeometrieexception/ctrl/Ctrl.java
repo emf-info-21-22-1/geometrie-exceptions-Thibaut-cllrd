7e8f4b19412c0aaae91f0aaed7484555651b2631
colliardt <colliardt@WSTEMFB32-02.stu.net.fr.ch>
2023-03-20 15:29:13 +0100

    Finito

--- src/exercicegeometrieexception/ctrl/Ctrl.java
+++ src/exercicegeometrieexception/ctrl/Ctrl.java
@@ -11,25 +11,49 @@ public class Ctrl implements ICtrlIhm {
     public void demarrer() {
         refIhm.demarrer();
     }
-    
+
     @Override
-    public void selectCalcRadius( String value ) {
-        double valueDouble = Double.parseDouble( value );
-        refIhm.afficheResultatCircle( String.valueOf( refWorker.calcRadiusCircleFromArea( valueDouble ) ) );
+    public void selectCalcRadius(String value) {
+        try {
+            double valueDouble = Double.parseDouble(value);
+            refIhm.afficheResultatCircle(String.valueOf(refWorker.calcRadiusCircleFromArea(valueDouble)));
+        } catch (NumberFormatException e) {
+            refIhm.afficheMessage("Faut entrer un chiffre mon gaillard");
+        } catch (IllegalArgumentException e) {
+            refIhm.afficheMessage("Faut entrer un chiffre positif mon gaillard");
+        }
     }
 
     @Override
-    public void selectCalcLargeur( String valueArea, String valueLongueur ) {
-        double valueAreaDouble = Double.parseDouble( valueArea );
-        double valueLongueurDouble = Double.parseDouble( valueLongueur );
-        refIhm.afficheResultatRectangle( String.valueOf( refWorker.calcLargeurFromRectangle( valueAreaDouble, valueLongueurDouble ) ) );
+    public void selectCalcLargeur(String valueArea, String valueLongueur) {
+        double valueAreaDouble = 0;
+        double valueLongueurDouble = 0;
+        try {
+            valueAreaDouble = Double.parseDouble(valueArea);
+
+            refIhm.afficheResultatRectangle(String.valueOf(refWorker.calcLargeurFromRectangle(valueAreaDouble, valueLongueurDouble)));
+        } catch (NumberFormatException e) {
+            refIhm.afficheMessage("Faut comprendre que tu peux pas mettre autre chose que des chiffres !");
+        } catch (IllegalArgumentException e) {
+            refIhm.afficheMessage("Faut comprendre que tu peux pas mettre du negatif !");
+        }
+
+        try {
+
+            valueLongueurDouble = Double.parseDouble(valueLongueur);
+            refIhm.afficheResultatRectangle(String.valueOf(refWorker.calcLargeurFromRectangle(valueAreaDouble, valueLongueurDouble)));
+        } catch (NumberFormatException e) {
+            refIhm.afficheMessage("Faut comprendre que tu peux pas mettre autre chose que des chiffres !");
+        } catch (IllegalArgumentException e) {
+            refIhm.afficheMessage("Faut comprendre que tu peux pas mettre du negatif !");
+        }
     }
 
-    public void setRefIhm( IIhmCtrl refIhm ) {
+    public void setRefIhm(IIhmCtrl refIhm) {
         this.refIhm = refIhm;
     }
 
-    public void setRefWorker( IWrkCtrl refWorker ) {
+    public void setRefWorker(IWrkCtrl refWorker) {
         this.refWorker = refWorker;
     }
 
